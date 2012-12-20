67e8db54fe0e01f6e0573b7211e0de11d8622867
cleversoap <clever@cleversoap.com>
2012-12-20 15:15:39 +1000

    Added single slot signal

---
+++ spsingle.hpp
@@ -0,0 +1,56 @@
+#ifndef __SPSINGLE_HPP__
+#define __SPSINGLE_HPP__
+
+#include "sigplus.hpp"
+
+template <typename...T>
+class SPSingle : public SigPlus<T...>
+{
+	public:
+		typedef typename SigPlus<T...>::SlotFunc SlotFunc;
+		typedef typename SigPlus<T...>::SlotDelegate SlotDelegate;
+
+		SPSingle(bool locked = false)
+		: _locked(locked)
+		{
+		}
+
+		virtual void operator() (T... args)
+		{
+			if (_slot != nullptr)
+				(*_slot)(args...);
+		}
+
+		virtual void connect(SlotFunc slotFunc)
+		{
+			if (!_locked)
+				_slot = &slotFunc;
+		}
+
+		virtual void remove(SlotFunc slotFunc)
+		{
+			if (slotFunc.template target<SlotDelegate*>() ==
+				static_cast<SlotFunc>(*_slot).template target<SlotDelegate*>())
+			{
+				clear();
+			}
+		}
+
+		virtual void removeAt(int i)
+		{
+			if (i == 0)
+				clear();
+		}
+
+		virtual void clear()
+		{
+			if (_slot != nullptr)
+				delete _slot;
+		}
+
+	protected:
+		bool _locked; 
+		SlotFunc* _slot;
+};
+
+#endif
